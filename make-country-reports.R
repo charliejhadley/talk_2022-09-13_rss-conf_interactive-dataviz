d00793b93e22c84cbcb5c0263ca8d9856fed5216
charliejhadley <charliejhadley@gmail.com>
2022-09-13 11:18:43 +0100

    pre-talk setup

---
+++ make-country-reports.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(quarto)
+
+seleted_countries <- c("United Kingdom", "Brazil", "Australia")
+
+seleted_countries %>%
+  walk(~quarto_render("country-report.qmd", output_file = str_glue("report_{.x}.html"), execute_params = list(country = .x)))
+
+quarto_render()
